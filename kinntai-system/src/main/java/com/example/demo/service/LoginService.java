a0ab52ff6765e07da7078bf19e10b7f5c525cba9
github Takumi-Haito <takumi-haito@kccs.co.jp>
2025-07-18 09:10:42 +0900

    エラー削減(hai)

--- kinntai-system/src/main/java/com/example/demo/service/LoginService.java
+++ kinntai-system/src/main/java/com/example/demo/service/LoginService.java
@@ -13,7 +13,7 @@ public class LoginService {
 
     public boolean authenticate(String userId, String rawPassword) {
         return userRepository.findById(userId)
-                .map(user -> user.getPassword().equals(rawPassword)) // 実際はハッシュ比較
+                .map(user -> user.getPass().equals(rawPassword)) // 実際はハッシュ比較
                 .orElse(false);
     }
 }
