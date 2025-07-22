ad4cd7805cbca2a4c2438e55878479b386ab7c58
Soshi-Takata <soshi-takata@kccs.co.jp>
2025-07-22 14:15:22 +0900

    Delete attendance directory

--- attendance/src/main/java/com/example/attendance/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.attendance.repository;
-
-import com.example.attendance.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-} 
