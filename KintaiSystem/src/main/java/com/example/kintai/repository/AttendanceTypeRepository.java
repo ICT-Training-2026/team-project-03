e770359de70c6f74df99cd45fe1fc3480d6149cc
Soshi-Takata <soshi-takata@kccs.co.jp>
2025-07-22 14:14:24 +0900

    Delete KintaiSystem directory

--- KintaiSystem/src/main/java/com/example/kintai/repository/AttendanceTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.kintai.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.kintai.entity.AttendanceType;
-
-public interface AttendanceTypeRepository extends JpaRepository<AttendanceType, String> {
-}
--- KintaiSystem/src/main/java/com/example/kintai/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.kintai.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.kintai.entity.Department;
-
-public interface DepartmentRepository extends JpaRepository<Department, String> {
-    // 特殊な検索が必要ならここにメソッド追加
-}
