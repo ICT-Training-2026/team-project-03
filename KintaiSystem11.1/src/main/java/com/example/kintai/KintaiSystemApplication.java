48edea755d17afd6b176459259fe39dc9a84a3ef
github Takumi-Haito <takumi-haito@kccs.co.jp>
2025-07-28 12:59:57 +0900

    wak

--- KintaiSystem11.1/src/main/java/com/example/kintai/KintaiSystemApplication.java
+++ KintaiSystem11.1/src/main/java/com/example/kintai/KintaiSystemApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(
-  exclude = SecurityAutoConfiguration.class
+  exclude = SecurityAutoConfiguration.class  // Spring Security の自動設定を外す
 )
 public class KintaiSystemApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(KintaiSystemApplication.class, args);
     }
